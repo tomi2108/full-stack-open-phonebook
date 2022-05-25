60afc15a2523ae2de4f47dafd7cb2f7c296c3478
TomasSanchezNegrette <tsancheznegrette@frba.utn.edu.ar>
2022-05-24 21:47:14 -0300

    ,

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,11 @@ module.exports = {
   env: {
     node: true,
     commonjs: true,
-    es2021: true,
+    es2022: true,
   },
   extends: "eslint:recommended",
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: "latest",  
   },
   rules: {
     eqeqeq: "error",
